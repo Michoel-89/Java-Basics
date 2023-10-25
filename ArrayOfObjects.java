83a310b31d198169f6e40191ef06c70249561dfe
michoel <michoelr111@gmail.com>
2023-10-25 15:56:40 -0400

    array of objects

---
+++ ArrayOfObjects.java
@@ -0,0 +1,15 @@
+public class ArrayOfObjects {
+
+    public static void main(String[] args) {
+
+        Food food1 = new Food("pizza");
+        Food food2 = new Food("hamburger");
+        Food food3 = new Food("hotdog");
+        
+        Food[] refridgerator = {food1, food2, food3};
+
+        System.out.println(refridgerator[0].name);
+        System.out.println(refridgerator[1].name);
+        System.out.println(refridgerator[2].name);
+    }
+}
