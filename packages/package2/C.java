210427a43f8a9b7c6572ea37a2bdd320c78c46cb
michoel <michoelr111@gmail.com>
2023-10-26 08:29:09 -0400

    access modifiers

---
+++ packages/package2/C.java
@@ -0,0 +1,10 @@
+package package2;
+import package1.*;
+public class C {
+    
+    public String publicMessage = "this is public";
+    String defaultMessage = "this is the default";
+    protected String protectedMessage = "this is protected";
+    private String privateMessage = "this is private";
+    
+}
