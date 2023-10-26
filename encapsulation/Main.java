abe05eac951be3dc8cebfaaa2ef8c04749f5fd0a
michoel <michoelr111@gmail.com>
2023-10-26 08:43:38 -0400

    add encapsulation

---
+++ encapsulation/Main.java
@@ -0,0 +1,17 @@
+package encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+        // Your Java code goes here
+
+        Car car = new Car("Chevrolet", "Camaro", 2021);
+
+        car.setYear(2022);
+
+        System.out.println(car.getMake());
+        System.out.println(car.getModel());
+        System.out.println(car.getYear());
+
+        // encapsulation makes classes private
+    }
+}
