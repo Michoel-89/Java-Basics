b47672f271b0d83b94026b9e47aa65a1f859e293
michoel <michoelr111@gmail.com>
2023-10-27 09:45:57 -0400

    add GUI

---
+++ GUI/Main.java
@@ -0,0 +1,21 @@
+package GUI;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        MyFrame frame = new MyFrame();
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.setResizable(false);
+        // frame.setTitle("gui");
+        // frame.setSize(420,420);
+        // frame.setVisible(true);
+
+        // ImageIcon image = new ImageIcon("GUI/java.jpg");
+        // frame.setIconImage(image.getImage());
+        // frame.getContentPane().setBackground(Color.GREEN);
+    }
+}
--- fileClass/Main.java
+++ fileClass/Main.java
@@ -1,19 +1,44 @@
 package fileClass;
 
-import java.io.File;
+import java.io.FileReader;
+// import java.io.File;
+// import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
-        File file = new File("fileClass/file.txt");
+        try {
+            FileReader reader = new FileReader("poem.txt");
+            int data = reader.read();
 
-        System.out.println(file.getAbsolutePath());
-        if (file.exists()) {
-            System.out.println("file exists");
-            file.delete();
-        } else {
-            System.out.println("file doesn't exist");
-        }
+            while (data != -1) {
+                System.out.print((char)data);
+                data = reader.read();
+            }
+            reader.read();
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+        }    
+        // File file = new File("fileClass/file.txt");
+
+        // System.out.println(file.getAbsolutePath());
+        // if (file.exists()) {
+        //     System.out.println("file exists");
+        //     file.delete();
+        // } else {
+        //     System.out.println("file doesn't exist");
+        // }
+        // try {
+        //     FileWriter writer = new FileWriter("fileClass/poem.txt");
+        //     writer.write("roses are read \nviolets are blue");
+        //     writer.close();
+        // }
+        // catch (Exception e) {
+
+        //     e.printStackTrace();
+        // }
     }
 }
